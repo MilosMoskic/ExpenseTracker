6fa411f6f57327593791b173f934ba1ff54246ea
Molee <milosmoskic147@gmail.com>
2023-01-05 22:08:33 +0100

    Add project files.

---
+++ ExpenseTracker/Controllers/DashboardController.cs
@@ -0,0 +1,46 @@
+﻿using ExpenseTracker.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Transactions;
+using ExpenseTracker.Models;
+
+namespace ExpenseTracker.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public DashboardController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<ActionResult> Index()
+        {
+            //Last 7 days
+            DateTime StartDate = DateTime.Today.AddDays(-6);
+            DateTime EndDate = DateTime.Today;
+
+            List<Models.Transaction> SelectedTransactions = await _context.Transactions
+                .Include(x => x.Category)
+                .Where(y => y.Date >= StartDate && y.Date <= EndDate)
+                .ToListAsync();
+
+            //Total Income
+            int TotalIncome = SelectedTransactions
+                .Where(i => i.Category.Type == "Income")
+                .Sum(j => j.Amount);
+            ViewBag.TotalIncome = TotalIncome.ToString("C0");
+
+            //Total Expense
+            int TotalExpense = SelectedTransactions
+                .Where(i => i.Category.Type == "Expense")
+                .Sum(j => j.Amount);
+            ViewBag.TotalExpense = TotalExpense.ToString("C0");
+
+            //Balance
+            int Balance = TotalIncome - TotalExpense;
+            ViewBag.Balance = Balance.ToString("C0");
+
+            return View();
+        }
+    }
+}
