c90e3b748fc4f2122bf40b94da30ac52d323f664
Molee <milosmoskic147@gmail.com>
2023-06-30 14:50:56 +0200

    Implemented FindCategory methof in Repository

--- ExpenseTracker/Repositories/CategoryRepository.cs
+++ ExpenseTracker/Repositories/CategoryRepository.cs
@@ -1,6 +1,21 @@
-﻿namespace ExpenseTracker.Repositories
+﻿using ExpenseTracker.Data;
+using ExpenseTracker.Interfaces;
+using ExpenseTracker.Models;
+
+namespace ExpenseTracker.Repositories
 {
-    public class CategoryRepository
+    public class CategoryRepository : ICategoryRepository
     {
+        private readonly AuthContext _context;
+
+        public CategoryRepository(AuthContext context)
+        {
+            _context = context;
+        }
+
+        public Category FindCategory(int id)
+        {
+            return _context.Category.Where(c => c.CategoryId == id).FirstOrDefault();
+        }
     }
 }
